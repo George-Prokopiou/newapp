838453744c2df445d6ac6d2a1885f54ba177d7ce
George Prokopiou <george.prokopiou.andreas@gmail.com>
2018-10-09 18:38:33 +0300

    various fixes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 class App extends Component {
+ constructor() {
+ super();
+ this.state={
+  data:null, 
+ } 
+  
+ }
+ getData() {
+  fetch('https://www.googleapis.com/youtube/v3/search?key=AIzaSyCD8nJj05or-n459LxRrR1kgDFabagdvTo&maxResults=5&part=snippet&q=motogp')
+  .then((resp)=>{
+   resp.json()})
+   .then((res)=>{
+   console.log(res);
+   this.setState({data:res.videos})
+   }) 
+ }
+  
+ componentWillMount() {
+  this.getData();
+ }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div> {
+       this.state.data ?
+       this.state.data.map((item)=>
+       <div>
+        <h3>{item.title}</h3> 
+        <h3>{item.releaseVideo}</h3>
+       </div>
+      )
+       :
+       <h3>Wait...data is fetching.</h3>
+
+      }
       </div>
     );
   }
