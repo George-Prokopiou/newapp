f8e4e1cf9aa2517936875144ee01cf50a839bade
George-Prokopiou <george.prokopiou.andreas@gmail.com>
2018-10-28 11:40:14 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
 
-
 class App extends Component {
   constructor() {
     super();
     this.state = {
       data: null,
+      iframe_url: ''
     }
   }
-
   getData() {
     fetch('https://www.googleapis.com/youtube/v3/search?key=AIzaSyCD8nJj05or-n459LxRrR1kgDFabagdvTo&maxResults=5&part=snippet&q=motogp')
       .then(response => response.json())
       .then(data => {
         console.log('vaggos', data.items);
-        this.setState({ data: data.items });
+        this.setState({
+          data: data.items,
+          iframe_url: 'https://www.youtube.com/embed/' + data.items[0].id.videoId
+
+        });
       });
   }
-
   componentWillMount() {
     this.getData();
   }
-
   render() {
     let photos = this.state.data ?
       this.state.data.map(function (item) {
@@ -33,16 +34,18 @@ class App extends Component {
         );
       })
       :
-      <p>aaa</p>;
+      <p> Wait...Fetching data.</p>;
 
 
     return (
+
       <div>
         {photos}
-        <iframe src="" frameborder="0"></iframe>
+        <iframe src={this.state.iframe_url}></iframe>
       </div>
     );
   }
 }
+
 
 export default App;
